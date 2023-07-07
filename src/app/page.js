03fb3ef04f4488420b0a081b6c2ba29bef2ae48d
NGFuture <78330887+NGFuture@users.noreply.github.com>
2023-07-06 22:06:27 -0700

    starts project

--- src/app/page.js
+++ src/app/page.js
@@ -9,35 +9,43 @@ export default function Home() {
           <code className="font-mono font-bold">src/app/page.js</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="relative flex flex-col items-center justify-center w-full max-w-5xl h-full px-4 mx-auto mt-24 lg:mt-0 lg:px-0 lg:flex-row">
+        <div className="card flex flex-col items-center justify-center w-full h-full p-8 bg-white rounded-xl shadow-lg dark:bg-neutral-800">
+
+          <h2 className="mb-4 text-2xl font-semibold text-center text-gray-800 dark:text-gray-100">
+            Test AWS and Google text-to-speech APIs
+          </h2>
+
+          <div className="flex items-center justify-center w-full h-full">
+            <input
+              type="text"
+              className="flex-1 h-10 px-4  text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline"
+              placeholder="Enter your text to convert to speech"
+            />
+
+            <button className="flex items-center justify-center w-1/6 h-10 ml-4 text-gray-100 bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:shadow-outline"> Submit </button>
+          </div>
+          <div className="flex items-center justify-center w-full h-full mt-4">
+            <div className="flex-1 h-10 px-4  text-base text-gray-700 placeholder-gray-600 focus:shadow-outline pt-2">
+              <p>Output will be displayed here</p>
+            </div>
+            <div className="play-button flex items-center justify-center w-1/6 h-10 ml-4 text-gray-100 bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:shadow-outline">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="{2}" d="M19 9l-7 7-7-7" />
+              </svg>
+            </div>
+            
+          </div>
+
+            
+
+        </div>
       </div>
+
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
